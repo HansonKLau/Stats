d7570053b2ffde4259b7a90a272929c8bcdbca99
Hanson <Hansonlau7@gmail.com>
2025-03-01 18:54:24 -0800

    got rid of garbage

--- kfold.py
+++ kfold.py
@@ -1,54 +1,5 @@
 import random
 
-# list of x
-# list of y
-
-
-# def kFold(k, x, y):
-
-#     # create k "even" sets of data
-
-#     indicies = []
-
-#     for i in range(len(x)):
-#         indicies.append(i)
-
-#     last = -1
-
-#     if len(x) % k != 0:
-#         last = len(x) - (k * (len(x) // k))
-#         5 - (3 * (5 // 3)) = 5 - (3 * 1)
-
-    
-#     xgroups = [] # list of groups of x
-
-#     ygroups = [] # list of groups of y
-
-#     for i in range(k):
-
-#         xgroup = []
-#         ygroup = []
-
-#         if i == k - 1 and last != -1:
-#             for j in range(last):
-#                 random_index = random.randrange(len(indicies))
-#                 num = indicies.pop(random_index)
-#                 xgroup.append(x[num])
-#                 ygroup.append(y[num])
-
-#         else:
-#             for j in range(len(x) // k):
-#                 random_index = random.randrange(len(indicies))
-#                 num = indicies.pop(random_index)
-#                 xgroup.append(x[num])
-#                 ygroup.append(y[num])
-        
-#         xgroups.append(xgroup)
-#         ygroups.append(ygroup)
-    
-#     return (xgroups, ygroups)
-
-
 def kFold(k, x, y):
 
     # create k "even" sets of data
